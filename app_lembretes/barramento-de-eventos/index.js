20edbac516b093e686894a2797b695cc87242965
asbonato <asbonato@gmail.com>
2025-04-26 11:58:45 -0300

    apostila_mss: aula 26/4 - até barramento - 4.4.34 retomar do mss de consulta - 4.3.35 - pag. 43

---
+++ app_lembretes/barramento-de-eventos/index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+const axios = require('axios')
+
+app.use(express.json())
+
+app.post('/eventos', (req, res) => {
+    const evento = req.body
+    //envia o evento para o microsserviço de lembretes
+    axios.post('http://localhost:4000/eventos', evento)
+    //envia o evento para o microsservico de observacoes
+    axios.post('http://localhost:5000/eventos', evento)
+    res.status(200).send({msg:'ok'})
+})
+
+app.listen(10000, () => {
+    console.log('Barramento de eventos. Porta 10000.')
+})
