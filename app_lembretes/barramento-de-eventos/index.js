089b9d70930a6832a8847419314da9045982584b
asbonato <asbonato@gmail.com>
2025-05-10 10:01:55 -0300

    apostila_mss: aula 10/05 - até barramento - 4.3.39     retomar do mss de observacoes - 4.3.40 - pag. 47

--- app_lembretes/barramento-de-eventos/index.js
+++ app_lembretes/barramento-de-eventos/index.js
@@ -10,6 +10,8 @@ app.post('/eventos', (req, res) => {
     axios.post('http://localhost:4000/eventos', evento)
     //envia o evento para o microsservico de observacoes
     axios.post('http://localhost:5000/eventos', evento)
+    //envia o evento para o microsservico de consulta
+    axios.post('http://localhost:6000/eventos', evento)
     res.status(200).send({msg:'ok'})
 })
 
